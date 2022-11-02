f5ab219b97608ad19d692b93e7b918416ebdf56f
vchub <vlad.chub@gmail.com>
2022-11-01 22:38:41 -0700

    aiMove

--- tictac/tictac.js
+++ tictac/tictac.js
@@ -1,50 +1,51 @@
 const pr = console.log;
 
-function initGame() {
+// current state
+const world = { board: null, player: null };
+
+function newGame() {
   // init cell
   document.querySelectorAll('.board .cell').forEach((c, i) => {
     c.setAttribute('id', i);
-    c.onclick = (e) => onclick(e, i);
+    c.onclick = (e) => onclick(i);
+    c.textContent = '';
   });
-  // pr(document.querySelector('#\\36'));
-  // pr(document.getElementById('6'));
 
-  // init state
-  newGame();
+  world.board = Array(9).fill(null);
+  world.player = 'O';
+  showStatus(world);
 }
 
-// current state
-let board, player;
-
-function newGame() {
-  board = Array(9).fill(null);
-  player = nextPlayer(player);
-  showStatus(board);
+function onclick(i) {
+  const world1 = makeMove(world, i);
+  if (world1 && !terminal(world.board)) {
+    setTimeout(() => aiMove(world1), 300);
+  }
 }
 
-function onclick(e, i) {
-  if (board[i] != null) return;
-  if (terminal(board)) return;
-  board[i] = player;
-  showMove(e, i, player);
+function makeMove(world, i) {
+  if (world.board[i] != null) return null;
+  if (terminal(world.board)) return null;
+  world.board[i] = world.player;
+  markCell(i, world.player);
 
   // pr('b', board);
-  player = nextPlayer(player);
-  showStatus(board);
+  world.player = nextPlayer(world.player);
+  showStatus(world);
+  return world;
 }
 
-function showMove(e, i, player) {
-  const el = e != null ? e.target : document.getElementById(String(i));
-  el.textContent = player;
+function markCell(i, player) {
+  document.getElementById(String(i)).textContent = player;
 }
 
-function showStatus(b) {
+function showStatus({ board, player }) {
   let s = `player ${player}`,
-    won = winner(b);
+    won = winner(board);
   if (won) {
     s += `, won ${won}`;
   }
-  if (terminal(b)) {
+  if (terminal(board)) {
     s += ', game over';
   }
 
@@ -93,4 +94,58 @@ function nextPlayer(p) {
   return p === 'O' ? 'X' : 'O';
 }
 
-// initGame();
+// find an optimum move and play
+function aiMove(w) {
+  const bestC = minMax(w.board, w.player);
+  return makeMove(w, bestC[1]);
+  // return randMove(w);
+}
+
+function randMove(w) {
+  const cs = filterNull(w.board);
+  const i = randSample(cs);
+  return makeMove(w, i);
+}
+
+function randSample(xs) {
+  const i = Math.floor(Math.random() * xs.length);
+  pr('i', i);
+  return xs[i];
+}
+
+function filterNull(b) {
+  return b.reduce((acc, c, i) => {
+    if (c == null) {
+      acc.push(i);
+    }
+    return acc;
+  }, []);
+}
+
+const scoreCache = new Map();
+const cacheKey = (b, p, i) => JSON.stringify([b, p, i]);
+
+// board, player, cell_i -> Number (score for move into cell_i)
+function score(b, p, i) {
+  // check cache
+  const k = cacheKey(b, p, i);
+  if (scoreCache.has(k)) return scoreCache.get(k);
+
+  if (terminal(b)) return utility(b);
+
+  const cs = filterNull(b),
+    b1 = [...b];
+  b1[i] = p;
+
+  const res = cs.reduce((acc, c) => (acc += score(b1, nextPlayer(p), c)), 0);
+  scoreCache.set(k, res);
+  return res;
+}
+
+// board, player -> cell with max score
+function minMax(b, p) {
+  const ss = filterNull(b).map((c) => [score(b, p, c), c]);
+  const res = ss.reduce((acc, x) => (acc[0] < x[0] ? x : acc));
+  pr('res', res);
+  return res;
+}
