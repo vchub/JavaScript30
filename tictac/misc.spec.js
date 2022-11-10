ca831734d0f17622d708ee5a9291377e785ce4bd
vchub <vlad.chub@gmail.com>
2022-11-09 22:29:13 -0800

    misc.spec

--- tictac/misc.spec.js
+++ tictac/misc.spec.js
@@ -8,6 +8,38 @@ describe('equality', () => {
   });
 });
 
+describe('tictac', () => {
+  it('terminal', () => {
+    let b = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    assert.equal(true, terminal(b));
+    b = ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'X', 'X'];
+    assert.equal(true, terminal(b));
+    b = [null, 'O', 'X', 'O', 'X', 'O', 'O', 'O', 'X'];
+    assert.equal(false, terminal(b));
+
+    b = Array(9).fill(null);
+    assert.equal(false, terminal(b));
+  });
+
+  it('winner and utlity', () => {
+    let b = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    assert.equal('X', winner(b));
+    assert.equal(1, utility(b));
+
+    b = ['O', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'O'];
+    assert.equal('O', winner(b));
+    assert.equal(-1, utility(b));
+
+    b = ['O', 'X', 'X', 'O', 'X', null, 'O', 'X', 'O'];
+    assert.equal('O', winner(b));
+    assert.equal(-1, utility(b));
+
+    b = ['O', 'X', 'X', 'X', 'X', null, 'O', null, 'O'];
+    assert.equal(null, winner(b));
+    assert.equal(0, utility(b));
+  });
+});
+
 // int -> [int]
 function* primes() {
   const res = [2];
@@ -55,34 +87,47 @@ describe('primes', () => {
   });
 });
 
-describe('tictac', () => {
-  it('terminal', () => {
-    let b = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-    assert.equal(true, terminal(b));
-    b = ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'X', 'X'];
-    assert.equal(true, terminal(b));
-    b = [null, 'O', 'X', 'O', 'X', 'X', 'X', 'X', 'X'];
-    assert.equal(false, terminal(b));
+// Jonson-Trotter algo
+// int -> [[int]]
+function permJ(n) {
+  if (n == 1) return [[1]];
+  return permJ(n - 1)
+    .map((p) => insert(p, n))
+    .flat(1);
+}
 
-    b = Array(9).fill(null);
-    assert.equal(false, terminal(b));
-  });
+// [int]-> [[int]]
+// insert x into xs on every index from 0 to len(xs)
+function insert(xs, x) {
+  const res = [];
+  for (let i = 0; i < xs.length + 1; i++) {
+    res.push(xs.slice(0, i).concat([x], xs.slice(i)));
+  }
+  return res;
+}
 
-  it('winner and utlity', () => {
-    let b = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-    assert.equal('X', winner(b));
-    assert.equal(1, utility(b));
+describe('perm', () => {
+  it('perm Jonson-Trotter', () => {
+    assert.deepEqual([[1]], permJ(1));
 
-    b = ['O', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'O'];
-    assert.equal('O', winner(b));
-    assert.equal(-1, utility(b));
+    assert.deepEqual(
+      new Set([
+        [1, 2],
+        [2, 1],
+      ]),
+      new Set(permJ(2)),
+    );
 
-    b = ['O', 'X', 'X', 'O', 'X', null, 'O', 'X', 'O'];
-    assert.equal('O', winner(b));
-    assert.equal(-1, utility(b));
-
-    b = ['O', 'X', 'X', 'X', 'X', null, 'O', null, 'O'];
-    assert.equal(null, winner(b));
-    assert.equal(0, utility(b));
+    assert.deepEqual(
+      new Set([
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1],
+      ]),
+      new Set(permJ(3)),
+    );
   });
 });
